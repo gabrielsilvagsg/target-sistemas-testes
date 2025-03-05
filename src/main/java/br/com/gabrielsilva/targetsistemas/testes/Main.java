70e5ed835ec89b32753e0cc5ddb133cddc93cb4a
gabrielsilvagsg <gabrielsilvagsg@hotmail.com>
2025-03-05 10:45:15 -0300

    Teste 01 e gitignore

--- src/main/java/br/com/gabrielsilva/targetsistemas/testes/Main.java
+++ src/main/java/br/com/gabrielsilva/targetsistemas/testes/Main.java
@@ -1,7 +1,21 @@
 package br.com.gabrielsilva.targetsistemas.testes;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        teste01();
+    }
+
+    public static void teste01() {
+        int INDICE = 13;
+        int SOMA = 0;
+        int K = 0;
+
+        while (K < INDICE) {
+            K = K + 1;
+            SOMA = SOMA + K;
+        }
+
+        System.out.println("Soma total: " + SOMA);
     }
 }
