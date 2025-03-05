15a02da3f241d2f0eba7292f61b3652b755aef79
gabrielsilvagsg <gabrielsilvagsg@hotmail.com>
2025-03-05 10:53:50 -0300

    Teste 02

--- src/main/java/br/com/gabrielsilva/targetsistemas/testes/Main.java
+++ src/main/java/br/com/gabrielsilva/targetsistemas/testes/Main.java
@@ -1,9 +1,12 @@
 package br.com.gabrielsilva.targetsistemas.testes;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
         teste01();
+        teste02();
     }
 
     public static void teste01() {
@@ -17,5 +20,34 @@ public class Main {
         }
 
         System.out.println("Soma total: " + SOMA);
+    }
+
+    public static void teste02() {
+        // Gerando um número aleatório ;)
+        int numero = new Random().nextInt(31);
+    
+        System.out.println("Número gerado: " + numero);
+    
+        if (pertenceAFibonacci(numero)) {
+            System.out.println(numero + " pertence à sequência de Fibonacci.");
+        } else {
+            System.out.println(numero + " não pertence à sequência de Fibonacci.");
+        }
+    }
+
+    public static boolean pertenceAFibonacci(int numero) {
+        int primeiro = 0, segundo = 1;
+
+        if (numero == primeiro || numero == segundo) {
+            return true;
+        }
+
+        while (segundo < numero) {
+            int proximoNumero = primeiro + segundo;
+            primeiro = segundo;
+            segundo = proximoNumero;
+        }
+
+        return segundo == numero;
     }
 }
